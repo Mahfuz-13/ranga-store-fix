20543d42e7ab3a963f3470baf91e5c9354bf6ead
Mahfuz-13 <mahfuzulh14@gmail.com>
2021-09-13 15:19:18 +0600

    total fixed

--- js/app.js
+++ js/app.js
@@ -30,8 +30,8 @@ let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
   updatePrice("price", price);
-
   updateTaxAndCharge();
+  updateTotal();
   document.getElementById("total-Products").innerText = count;
 };
 
@@ -46,7 +46,7 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText =  total.toFixed(2);
 };
 
 // set innerText function
